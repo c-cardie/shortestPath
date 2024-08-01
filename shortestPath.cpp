d4a3febcd32f52d5430faae2eceb492f848a7791
c-cardie <ccardie001@student.franciscan.edu>
2024-08-01 15:52:50 -0400

    made an alias for graph type

--- shortestPath.cpp
+++ shortestPath.cpp
@@ -2,21 +2,13 @@
 #include <vector>
 using namespace std;
 
-int main() {
+using graph = vector<vector<int>>;
 
-vector<vector<int>> graph = {
 
-    {1,2},
-    {3,5},
-    {3},
-    {},
-    {1},
-    {3,4}
+//function to test if graph is initialized correctly
+void printGraph(graph& graph) {
 
-};
-
-//testing if graph is initialized correctly
-for (int i = 0; i <= graph.size() - 1; i++) {
+  for (int i = 0; i <= graph.size() - 1; i++) {
 
     cout << "node " << i << "   neigbors ";
 
@@ -36,9 +28,50 @@ for (int i = 0; i <= graph.size() - 1; i++) {
 
     cout << endl;
 
+    }  
+
 }
 
+//function to test if visited has the correct number of slots based on the graph
+void printVisited(vector<bool>& visited) {
+
+    for (int i = 0; i <= visited.size() - 1; i++ ) {
+
+        cout << visited[i] << endl;
+
+    }
+
+}
+
+void findAllPaths() {
+
+    //Add the start node to the current path and mark it as visited
+    //path.push_back()
+
+
+}
+
+int main() {
+
+graph graph = {
+
+    {1,2},
+    {3,5},
+    {3},
+    {},
+    {1},
+    {3,4}
+
+};
+
+//testing if graph is initialized correctly
+printGraph(graph);
+
 vector<bool> visited(graph.size());
+
+
+//testing to see if visited has the correct number of slots based on the graph
+printVisited(visited);
 
 vector<vector<int>> allPaths;
 
