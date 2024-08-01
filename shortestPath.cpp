1419cde5216243d68ef63c549c32858f84f974a7
c-cardie <ccardie001@student.franciscan.edu>
2024-08-01 16:33:59 -0400

    successfully finding paths and denoting when there is not any

--- shortestPath.cpp
+++ shortestPath.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 using graph = vector<vector<int>>;
+using visited = vector<bool>;
+using path = vector<int>;
 
 
 //function to test if graph is initialized correctly
@@ -43,11 +45,64 @@ void printVisited(vector<bool>& visited) {
 
 }
 
-void findAllPaths() {
+void findAllPaths(graph& graph, int start, int end, path& path, visited& visited, vector<vector<int>>& allPaths) {
+
+    //Base case: if start equals end, add the end node to the path
+    //and store the path in allPaths
+
+    if (start == end) {
+        path.push_back(end);
+        allPaths.push_back(path);
+        path.pop_back();
+        return; //end current function call
+    }
 
     //Add the start node to the current path and mark it as visited
-    //path.push_back()
+    path.push_back(start);
+    visited[start] = true;
 
+    //iterate over all the neighbors of the current start node
+    for (auto &neighbor : graph[start]) {
+
+        //if the neighbor has not been visited, recursively call findAllPaths
+        //with the neighbor as the new "start"
+        if (!visited[neighbor]) {
+
+            findAllPaths(graph, neighbor, end, path, visited, allPaths);
+        }
+
+    }
+
+    //Unmark the start node as visited and remove it from the current path (unwinding)
+    visited[start] = false;
+    path.pop_back();
+
+}
+
+void printAllPaths(vector<vector<int>>& allPaths) {
+
+    if (allPaths.empty()) {
+
+    cout << "no paths found" << endl;
+
+    }
+
+    else {
+        for (int i = 0; i <= allPaths.size() - 1; i++) {
+
+            cout << "path " << i << ":   ";
+
+            for(int j = 0; j <= allPaths[i].size() - 1; j++) {
+
+            cout << allPaths[i][j] << " ";
+
+            }
+
+            cout << endl;
+
+        }
+
+    }
 
 }
 
@@ -65,14 +120,21 @@ graph graph = {
 };
 
 //testing if graph is initialized correctly
-printGraph(graph);
+//printGraph(graph);
+
+path path;
 
 vector<bool> visited(graph.size());
 
 
 //testing to see if visited has the correct number of slots based on the graph
-printVisited(visited);
+//printVisited(visited);
 
 vector<vector<int>> allPaths;
+
+findAllPaths(graph, 0, 3, path, visited, allPaths);
+
+//print all paths
+printAllPaths(allPaths);
 
 }
